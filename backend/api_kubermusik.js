05ea8875f5d520c8552ef69eb5e0cbaa5e742ad2
Roberto <rgg.correo@gmail.com>
2020-04-09 15:32:12 -0500

    adding gitignore, and making work queris

--- backend/api_kubermusik.js
+++ backend/api_kubermusik.js
@@ -3,125 +3,138 @@ API para conectar la base de datos al front end usando mongoose
 */
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 app.use(express.json());
 
-var mongoose = require('mongoose');
-
 //conection + error handling
-const mongodb = 'mongodb+srv://Test:uhPoUIbK2lpNVHRm@cluster0-vnehv.gcp.mongodb.net/test?retryWrites=true&w=majority'; //miss
-mongoose.connect(mongodb, {
-    useUnifiedTopology: true, useNewUrlParser: true
-})
-.then(() => console.log('Connected to the DB!'))
-.catch(err => {console.log(`DB connection Error: ${err.message}`);
-});
+const mongodb = 'mongodb+srv://Test:uhPoUIbK2lpNVHRm@cluster0-vnehv.gcp.mongodb.net/Tarea3BDAvanzadas'; //miss
+mongoose
+	.connect(mongodb, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true
+	})
+	.then(() => {
+		console.log('Connected to the DB!');
+		//check collections
+		//console.log(Object.keys(mongoose.connection.collections));
+	})
+	.catch((err) => {
+		console.log(`DB connection Error: ${err.message}`);
+	});
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
+//const db = mongoose.connection;
+//db.on('error', console.error.bind(console, 'connection error:'));
 
 //////////////////////////////ENDPOINTS/////////////////////////////
 
 //Pulling the models needed for the endpoints (all of them, basically)
 
 //NOTA: NO HE ENCONTRADO LA MANERA DE SEGMENTAR EL CODIGO.
-// var albums = require('./models/albums');
-// var artists = require('./models/artists');
-// var companies = require('./models/companies');
-// var songs = require('./models/songs');
+// let albums = require('./models/albums');
+// let artists = require('./models/artists');
+// let companies = require('./models/companies');
+// let songs = require('./models/songs');
 
 ///////////////////////////// SCHEMAS ///////////////////////////
-var Schema = mongoose.Schema;
-var albumSchema = new Schema({
-    name: String,
-    launch_date: Date,
-    id_company: Int32Array,
-    id_artist: Int32Array
+let Schema = mongoose.Schema;
+let albumSchema = new Schema({
+	name: String,
+	launch_date: Date,
+	id_company: Number,
+	id_artist: Number
 });
 
-var artistSchema = new Schema({
-    name: String,
-    start_date: Date,
-    birth_date: Date,
-    birth_country: String
+let artistSchema = new Schema({
+	name: String,
+	start_date: Date,
+	birth_date: Date,
+	birth_country: String
 });
 
-var companiesSchema = new Schema({
-    name: String,
-    start_date: Date,
-    coordinates: {
-        type: {
-            type: String, // Don't do `{ location: { type: String } }`
-            enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
-        },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
-    }
+let companiesSchema = new Schema({
+	name: String,
+	start_date: Date,
+	coordinates: {
+		type: {
+			type: String, // Don't do `{ location: { type: String } }`
+			enum: [ 'Point' ], // 'location.type' must be 'Point'
+			required: true
+		},
+		coordinates: {
+			type: [ Number ],
+			required: true
+		}
+	}
 });
-var songSchema = new Schema({
-    name: String,
-    duration: Int32Array,
-    genres: [String],
-    id_artist: Int32Array,
-    id_album: Int32Array,
+let songSchema = new Schema({
+	name: String,
+	duration: Number,
+	genres: [ String ],
+	id_artist: Number,
+	id_album: Number
 });
 
-mongoose.model("artist", artistSchema, 'artists');
-mongoose.model("album", albumSchema, 'albums');
-mongoose.model("company", companiesSchema, 'companies');
-mongoose.model("song", songSchema, 'songs');
-
-
+const artistsCollection = mongoose.model('artist', artistSchema, 'artists');
+mongoose.model('album', albumSchema, 'albums');
+mongoose.model('company', companiesSchema, 'companies');
+mongoose.model('song', songSchema, 'songs');
 
 //////////////////////////////////////ENDPOINTS//////////////////////////
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+	res.send('Hello World');
+	// si no haces el limit tarda un buen
+	/*artistsCollection.find({}).limit(10).exec((err, data) => {
+		if(err){
+		console.log(err)
+		}
+		console.log(data)
+	} )*/
 });
 
 app.get('/api/artists', (req, res) => {
-    db.artists.aggregate([
-        {
-            '$match': {}
-        }, {
-            '$sort': {
-                'name': 1
-            }
-        }, {
-            '$limit': 30
-        }
-    ]);
+	artistsCollection
+		.aggregate([
+			{
+				$sort: {
+					name: 1
+				}
+			},
+			{ $limit: 30 }
+		])
+		.exec((err, data) => {
+			if (err) {
+				console.log(err);
+			}
+			//console.log(data);
+			res.send(data)
+		});
 });
 
-app.get('/api/artists/:id', (req,res) => {
-    res.send(req.params);
+app.get('/api/artists/:id', (req, res) => {
+	res.send(req.params);
 });
 
 // app.get('/api/artists/:id', (req, res) => {
 //     res.send(req.params);
 // });
 
-app.get('/api/songs/', (req, res) =>{
-    var song = mongoose.model("song", songSchema, 'songs');
-    song.find({name:'Pearl'}, 'name', function (err, result) {
-        if (err) return handleError(err);
-        else res.send(result);
-        // Prints "Space Ghost is a talk show host".
-        // console.log('%s %s is a %s.', person.name.first, person.name.last,
-        //     person.occupation);
-    });
+app.get('/api/songs/', (req, res) => {
+	let song = mongoose.model('song', songSchema, 'songs');
+	song.find({ name: 'Pearl' }, 'name', function(err, result) {
+		if (err) return handleError(err);
+		else res.send(result);
+		// Prints "Space Ghost is a talk show host".
+		// console.log('%s %s is a %s.', person.name.first, person.name.last,
+		//     person.occupation);
+	});
 });
-
-
 
 // app.get('/api/songs/', (req,res) =>{
 //     db.songs.find({name: 'Forever Carousel'}, 'name', function (err, result) {
-//         if (err) return handleError(err); 
+//         if (err) return handleError(err);
 //         else res.send(result);
 //     })
 // });
@@ -130,4 +143,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // require module that you need to pull with
-// var SomeModel = require('../models/somemodel')
+// let SomeModel = require('../models/somemodel')
